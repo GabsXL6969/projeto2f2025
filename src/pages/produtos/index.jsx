68f446ab4f6f07e04c752cb369d159be1362a045
GabsXL6969 <rm95287@estudante.fieb.edu.br>
2025-09-19 08:32:06 -0300

    consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,10 +1,11 @@
+import axios from "axios"
 const Produtos = () => {
-    // Objeto com a lista de pizzas
-    const pizzas = [
-        'Pizza de Muçarela',
-        'Pizza de Calabresa',
-        'Pizza Baiana'
-    ]
+    // Consumo de um endpoint com as pizzas
+    axios.get("http://172.19.0.49/pizzariaoficial/api/v1/index")
+    .then(response=>{
+        console.log(response.data)
+    })
+    
     // Interação da lista de pizzas
     const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
 
